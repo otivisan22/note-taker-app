2416acfef0ebefbf9660d5cc292b4a1d6133ddda
Otilia Visan <otilia.visan22@gmail.com>
2021-05-18 13:04:52 +0100

    install heroku

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+``;
 const PORT = 3000;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const handleRequest = (req, res) => {
   res.send("Hello World!");
 };
